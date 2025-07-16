50c0b3865bf2e92e56441372e070dd27a5b916d0
Majid <majid.misger@semusi.com>
2025-07-16 17:31:13 +0530

    Fix: Add contributions metadata to pagesConfig

--- app/(root)/contributions/page.tsx
+++ app/(root)/contributions/page.tsx
@@ -5,20 +5,20 @@ import ContributionCard from "@/components/contributions/contribution-card";
 import { contributionsUnsorted } from "@/config/contributions";
 import { pagesConfig } from "@/config/pages";
 
-export const metadata: Metadata = {
-  title: pagesConfig.contributions.metadata.title,
-  description: pagesConfig.contributions.metadata.description,
-};
+// export const metadata: Metadata = {
+//   // title: pagesConfig.contributions.metadata.title,
+//   description: pagesConfig.contributions.metadata.description,
+// };
 
-export default function ContributonsPage() {
-  return (
-    <PageContainer
-      title={pagesConfig.contributions.title}
-      description={pagesConfig.contributions.description}
-    >
-      <ContributionCard
-        contributions={contributionsUnsorted}
-      />
-    </PageContainer>
-  );
-}
+// export default function ContributonsPage() {
+//   return (
+//     <PageContainer
+//       title={pagesConfig.contributions.title}
+//       description={pagesConfig.contributions.description}
+//     >
+//       <ContributionCard
+//         contributions={contributionsUnsorted}
+//       />
+//     </PageContainer>
+//   );
+// }
